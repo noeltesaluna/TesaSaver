c4cf3129c16d03f4943766962858790ea25022a3
noeltesaluna <ntesaluna03@gmail.com>
2025-03-10 15:51:43 +1100

    implemented update route for budget

--- api/controllers/budgetController.js
+++ api/controllers/budgetController.js
@@ -15,3 +15,23 @@ exports.getAllBudgets = async (req, res) => {
         res.status(500).json({ error: error });
     }
 };
+
+exports.updateBudget = async (req, res) => {
+    const id = req.params.id;
+    const { budget_name, budget_amount, start_date, end_date } = req.body;
+    try {
+        const budget = await Budget.findByPk(id)
+        if (!budget) {
+            res.status(404).json({ error: 'Todo not found' });
+        } else {
+            budget.budget_name = budget_name
+            budget.budget_amount = budget_amount
+            budget.start_date = start_date
+            budget.end_date = end_date
+            await budget.save()
+            res.json(budget)
+        }
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+};
--- api/routes/budgetRoutes.js
+++ api/routes/budgetRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const budgetContoller = require('../controllers/budgetController')
 
 router.get('/', budgetContoller.getAllBudgets)
+router.put('/:id', budgetContoller.updateBudget)
 
 module.exports = router
