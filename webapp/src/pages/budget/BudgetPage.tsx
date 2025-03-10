85fb34f8a616df8ddbb890dcf67e2ed7ff186eff
noeltesaluna <ntesaluna03@gmail.com>
2025-03-10 18:40:13 +1100

    budget page now works with api

--- webapp/src/pages/budget/BudgetPage.tsx
+++ webapp/src/pages/budget/BudgetPage.tsx
@@ -1,13 +1,23 @@
 import BudgetAmount from "./BudgetAmount"
-import Bucket from "./Bucket"
+import BucketComponent from "./BucketComponent"
+import useFetch from "../../api/useFetch"
+import { useState } from "react"
+import type { Budget } from "../../types/types"
 const Budget = () => {
-  return (
+    const {data, error} = useFetch('budgets/1')
+
+    const budget = data as Budget
+    const {Buckets, budget_name} = budget
+    
+    console.log(data)
+    return (
         <div className="flex min-w-dvw justify-center pt-16">
             <div className="flex flex-col min-w-3xl gap-4">
-                <h1 className="text-5xl font-bold text-blue-500">Budget</h1>
+                <h1 className="text-5xl font-bold text-blue-500">{budget_name}</h1>
                 <p>Let's get started with your budget.</p>
                 <BudgetAmount amount={10}/>
-                <Bucket bucketname={'Needs'} amount={10}/>
+                {Buckets?.map((bucket) => <BucketComponent {...bucket}/>)}
+                
             </div>
         </div>
     )
